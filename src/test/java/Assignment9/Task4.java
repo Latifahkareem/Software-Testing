ff75f25286b7d4151165cecc09fc2b5e3879b7f0
Latifah Kareem <36197886+Latifahkareem@users.noreply.github.com>
2025-09-28 06:14:26 +0300

    Update Task4.java

--- src/test/java/Assignment9/Task4.java
+++ src/test/java/Assignment9/Task4.java
@@ -22,20 +22,19 @@ public class Task4 {
 
     @Test
     public void positiveLoginTest() {
-        // إدخال بيانات صحيحة
+
         driver.findElement(By.id("username")).sendKeys("admin");
         driver.findElement(By.id("password")).sendKeys("123");
         driver.findElement(By.id("loginButton")).click();
 
-        // Hard Assertion 1: تحقق من URL
         String expectedUrl = "https://claruswaysda.github.io/signIn.html";
         String actualUrl = driver.getCurrentUrl();
-        Assert.assertEquals(actualUrl, expectedUrl, "ERROR: URL بعد تسجيل الدخول غير صحيح!");
+        Assert.assertEquals(actualUrl, expectedUrl, "ERROR: after login URL not true!");
 
         // Hard Assertion 2: تحقق من وجود نص "Employee Table"
         WebElement body = driver.findElement(By.tagName("body"));
         Assert.assertTrue(body.getText().contains("Employee Table"),
-                "ERROR: الصفحة لا تحتوي على نص 'Employee Table'");
+                "ERROR: this page not contain 'Employee Table'");
     }
 
     @AfterMethod
